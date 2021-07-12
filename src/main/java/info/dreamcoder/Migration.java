5c1c79558f118c4c768ab6ad53a589ee08233a91
jimxl <tianxiaxl@gmail.com>
2021-07-12 23:56:04 +0800

    添加了依赖的jdbc，准备添加数据库相关的功能

--- src/main/java/info/dreamcoder/Migration.java
+++ src/main/java/info/dreamcoder/Migration.java
@@ -6,16 +6,11 @@ package info.dreamcoder;
  */
 public class Migration  {
 
-
     public static Table createTable(String name, ITableRun run) {
         Table table = new Table(name);
         run.action(table);
         return table;
     }
 
-    public static void main( String[] args ) {
-        createTable("test_table", (t) -> {
-            t.string("test_string_column");
-        });
-    }
+//    TODO: 添加数据库连接以及真实的创建表的方法
 }
